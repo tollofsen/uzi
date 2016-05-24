703f7eb30be9c1cae42706f6e500bf796d999ace
Jens Tollofsén <jens@tollofsen.se>
2016-05-24 20:08:16 +0200

    Updated speedwalks for OC.

--- modules/speedies.m
+++ modules/speedies.m
@@ -114,41 +114,26 @@
 ;  /quote -S /grepsw `/list -I -s alias_body_\.%{1}*
 
 
-;/alias .alchor sswsseeseesese
+/set _speedwalk_oc_tomyrridon Kings_Castle_Obelisk Portal_to_Ship 3wnwne2nw2nwne2nwn3en2es2enen5e
+/set _speedwalk_oc_alchor Elven_forest_Obelisk Alchor 2sw2s2es2esese
 /set _speedwalk_oc_antriad Elven_forest_Obelisk Antriad 2sw2s2es2e2n
-/set _speedwalk_oc_argo Sundhaven_Obelisk Argonaut wwnnnnw%;open gate%;wwnuwwwnn%;open cover%;dddde
-/set _speedwalk_oc_deep Alchor_Obelisk Orc_Caves 7seu2nw4n3e3n3u6end
-/set _speedwalk_oc_ef East_Turning_Point Enchanted_Forest 4en2en3es3enwne2nw2nwne2nwne3sesw2se2swse2s9w4n
-/set _speedwalk_oc_enfans New_Thalos_Obelisk Enfans 2e2n2e2nes2ene3n2wnw
-/set _speedwalk_oc_es Outside_West_Gate Earth_Sea wwwnwwwswswnwwwwwwwwwwwws2w2sen
-/set _speedwalk_oc_fortress Ultima_Obelisk Fortress_of_Mustain 3ndne2n4e2s
-/set _speedwalk_oc_gkp Market_Square Great_Knight_Paladin 3w5se4se
-/set _speedwalk_oc_gehenna Market_Square Gehenna wwwssssssssswsssssssss
-/set _speedwalk_oc_gremlin Kings_Castle_Obelisk Gremlin wwwnwnennwnnwnesssessnnnnn
+/set _speedwalk_oc_deep Sundhaven_Obelisk Abandoned_Caves 7seu2nw4n3e3n3u6end
+/set _speedwalk_oc_orshingal New_Thalos_Obelisk Orshingal 2en2e2nes2enen
+/set _speedwalk_oc_es Market_Square Earth_Sea 10wn3wswswn12ws2w2sen
+/set _speedwalk_oc_mustaine Ultima_Obelisk Fortress_of_Mustain 3ndne2n4e2s
+/set _speedwalk_oc_redfernes Fountain_Square Great_Knight_Paladin 4w2s18u
+/set _speedwalk_oc_gehenna Market_Square Gehenna 5s3w3sw9s
+/set _speedwalk_oc_turvagar Kings_Castle_Obelisk Turvagar 3wnwne2nw2nwne3sen
 /set _speedwalk_oc_highlands Kings_Castle_Obelisk Highlands 3wnw2ne2nw2nwne2nw3nw4nw2n
-/set _speedwalk_oc_imps Ultima_Obelisk Upper_Argo nnndnnnnnundnnn
-;/set _speedwalk_oc_impstp eeeeneeneeeseeenwnennwnnwnesssswswnundnnn
-/set _speedwalk_oc_ingelstone Ofcol_Obelisk Ingelstone wwssssssswwsssswwwwwwwwwwwswnnnnnnw
+/set _speedwalk_oc_argo Ultima_Obelisk Upper_Argo 3nd5nund3n
+/set _speedwalk_oc_inglestone Ofcol_Obelisk Inglestone 2w7s2w3s11wnw5nwn
 /set _speedwalk_oc_kender Ofcol_Obelisk Kender 2w7s2w3s8w2nen
-/set _speedwalk_oc_kerofk Outside_West_Gate Kerofk 3wn3wswswn3w2ne2nene2nwne2nes6e4n2en
-/set _speedwalk_oc_kings East_Turning_Point Kings_Castle eeeeneeneeeseeeeeen
-/set _speedwalk_oc_lagamor Outside_West_Gate Lagamor 3w9sw2sw3ses2wsw
-/set _speedwalk_oc_lorca Kings_Castle Almost_Lorca 3wnwnennwnnw
-/set _speedwalk_oc_lorca2 Almost_Lorca Lorca(DON'T_speedwalk) wn5ws2wnennneeneeseeeeeeesen
-/set _speedwalk_oc_minotaur Kings_Castle Minotaur wwwnwnennwnnwnesssesswsssdsssssses
-;/set _speedwalk_oc_pharao . . 2s2e2s5es2es3es2eses9en6enwne3ne2n
-;/set _speedwalk_oc_pharao2 . . 4w5n3enu4n4u%;open tomb%;2n2wune4neu5nu
-/set _speedwalk_oc_shadow Valley_of_Highlands Shadow 2dws7wu3wd2sws2e2d2esen2e2sn3e2s3en
-/set _speedwalk_oc_ship Kerofk_Obelisk Shipwreck s2en%;open cloth%;n%;open trapdoor%;d2e3de2n2ene2n3w
-/set _speedwalk_oc_soul2 2nd_Floor_Soulcrusher Agro_Soulcrushers ssnesseneeeswsese
-/set _speedwalk_oc_sundhaven East_Turning_Point Sundhaven 4en2en3es3enwne2nw2nwne2nw3nw4nwnwn4wn2wn6w
-/set _speedwalk_oc_taon Outside_West_Gate Halfway_to_Taon 3wn3wswswn3wse2sw2ses4w2s7e7n
-/set _speedwalk_oc_taon2 Halfway_to_Taon Taon wwwwwwssssssuueeennnnwwwwuuueeennuuue
-/set _speedwalk_oc_titstat New_Thalos Titanium_Statue eeneenneseeneeseseesd
-/set _speedwalk_oc_titstatms Market_Square Titanium_Statue 3w9sw2sw3ses2wsw2sw2ses2en2eses3esd
-/set _speedwalk_oc_withness Outside_West_Gate Demon_City 3wn3wswswn3wse2sw2se2sesen
-/set _speedwalk_oc_yggdrasil Outside_West_Gate Yggdrasil wwwnwwwswswnwwwwwwwwwwwwswwsswswwwnwn
-/set _speedwalk_oc_tomyrridon Turning_Point Portal_to_Ship 4en2en3es3enwne2nw2nwne2nwn3en2es2enen5e
+/set _speedwalk_oc_lagamor New_Thalos_Obelisk Lagamor 2en2e4ne2nwn2w
+/set _speedwalk_oc_lorca Kings_Castle Lorca 3wnwne2nw2n2wn5ws2wn
+/set _speedwalk_oc_mahntor Kings_Castle Mahn-Tor 3wnwne2nw2nwne4swse3sd4se2s2u
+/set _speedwalk_oc_gsoul 2nd_Floor_Soulcrusher Agro_Soulcrushers ssnesseneeeswsese
+/set _speedwalk_oc_trees Market_Square Haon-Dor 10wn3wswswn12w
+/set _speedwalk_oc_coven New_Thalos_Obelisk The_Coven 2enesd
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;Myrridon Speedwalks
