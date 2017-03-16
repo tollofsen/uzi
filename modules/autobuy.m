fa91ec129f1653f365f6b7ec1af143aa57c3262b
Jens Tollofsén <jens@tollofsen.se>
2017-03-16 23:38:49 +0100

    Handle CR-office breaking down.

--- modules/autobuy.m
+++ modules/autobuy.m
@@ -11,3 +11,12 @@
 
 /def uzi_autocr_buy = \
     buy corpse
+
+/def -mregexp -F -t"^(Sheila|The secretary) says 'Sorry ([A-z]+), we're booked full at the moment! Come back later.'" autobuy_2 = \
+    /if ({P2}=~char) \
+        /repeat -60 1 buy corpse%;\
+    /endif
+
+/def -mregexp -F -t"^(Shiela|the secretary) says 'Please form an orderly queue and ask for CR again.'" autobuy_3 = \
+    buy corpse
+
