eb91d8a1563c49765673d1563ca6570833ebba62
Jens Tollofsén <jens@tollofsen.se>
2017-10-15 11:09:17 +0200

    Slight fix to autodep module

--- modules/autodep.m
+++ modules/autodep.m
@@ -3,7 +3,7 @@
 /def -mregexp -Fp100 -t'^A Large device from the Lords of Orhan has been put here.$' uzi_autodep0 = \
     /if (gold>autodep_amount & autodep_amount>100000) \
         deposit $[gold-autodep_amount]%;\
-        /set gold=autodep_amount%;\
+        /set gold=%{autodep_amount}%;\
     /elseif (gold<100000) \
         withdraw 100000%;\
         /set gold=$[gold+100000]%;\
