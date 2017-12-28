b11ff59877320202958d09cb7f295e9a0c98df3a
Jens Tollofsén <jens@tollofsen.se>
2017-12-28 20:42:40 +0100

    Initial triggers for keeping track of the current zone/continent

--- modules/zones.m
+++ modules/zones.m
@@ -1,84 +1,104 @@
+/def set_zone = \
+    /set zone=%{*}
+
 ;; OC
 /def -F -msimple -t'The landing-stage of the inter-continental ferry' uzi_zones_fishing_village_docks = \
     /set hometown=Karandras%;\
+    /set_zone Fishing Village%;\
     /weapon slayhuman%;\
     /d normal
 
 /def -F -msimple -t'The Temple of Karandras' uzi_zones_karandras_temple = \
+    /set zone=Karandras%;\
     /set hometown=Karandras
 
 /def -F -msimple -t'A Small, Slithering Path In The Mountains' uzi_zones_slithering_obelisk = \
+    /set_zone Enchanted Forest%;\
     /set hometown=Karandras%;\
     /weapon slaydragon%;\
     /d normal
 
 /def -F -msimple -t'Entrance to the High Tower' uzi_zones_high_tower_obelisk = \
+    /set_zone High Tower%;\
     /set hometown=Karandras%;\
     /weapon slayhuman%;\
     /d normal
 
 /def -F -msimple -t'Entrance to the Elven forest' uzi_zones_elven_village_obelisk = \
+    /set_zone Elven Village%;\
     /set hometown=Karandras%;\
     /weapon slayelf unlife iron dark%;\
     /d slayelf unlife dark
 
 /def -F -msimple -t'Entrance to Elemental Canyon' uzi_zones_elemental_canyon_obelisk = \
+    /set_zone Elemental Canyon%;\
     /set hometown=Karandras%;\
     /weapon normal%;\
     /d normal
 
 /def -F -msimple -t'Street of the Gods, Silence' uzi_zones_underdark_obelisk = \
+    /set_zone Underdark%;\
     /set hometown=Karandras%;\
     /weapon normal%;\
     /d normal
 
 /def -F -msimple -t'Entrance to Atlantis' uzi_zones_atlantis_obelisk = \
+    /set_zone Atlantis%;\
     /set hometown=Karandras%;\
     /weapon slaymythical%;\
     /d normal
 
 /def -F -msimple -t'Outside the City Gates' uzi_zones_drow_city_obelisk = \
+    /set_zone Drow City%;\
     /set hometown=Karandras%;\
     /weapon slaydrowelf pure light ice%;\
     /d slaydrowelf pure light ice
 
 /def -F -msimple -t"Entrance to the King's Castle" uzi_zones_kings_castle_obelisk = \
+    /set_zone Kings Castle%;\
     /set hometown=Karandras%;\
     /weapon slayhuman%;\
     /d slayhuman normal
 
 
 /def -F -msimple -t'The top of a small hill' uzi_zones_unknown_obelisk = \
+    /set_zone Desolate Plains%;\
     /set hometown=Karandras%;\
     /weapon unlife%;\
     /d unlife
 
 /def -F -msimple -t'Shrine to the Emperor' uzi_zones_kerofk_obelisk = \
+    /set_zone Kerofk%;\
     /set hometown=Karandras%;\
     /weapon slayhuman%;\
     /d normal
 
 /def -F -msimple -t'End of the Main Road' uzi_zones_solace_obelisk = \
+    /set_zone Solace%;\
     /set hometown=Karandras%;\
     /weapon slayhuman%;\
     /d normal
 
 /def -F -msimple -t'At the gallows' uzi_zones_sundhaven_obelisk = \
+    /set_zone Sundhaven%;\
     /set hometown=Karandras%;\
     /weapon normal%;\
     /d normal
 
 /def -F -msimple -t'The Grand Gate of Thalos' uzi_zones_thalos_obelisk = \
+    /set_zone Thalos%;\
     /set hometown=Karandras%;\
     /weapon normal%;\
     /d normal
 
 /def -F -msimple -t'The Temple of Olympus' uzi_zones_olympus_obelisk = \
+    /set_zone Olympus%;\
     /set hometown=Karandras%;\
     /weapon slaymythical%;\
     /d normal
 
 /def -F -msimple -t'The Big Intersection' uzi_zones_ofcol_obelisk = \
+    /set_zone Ofcol%;\
     /set hometown=Karandras%;\
     /weapon slayhuman%;\
     /d normal
